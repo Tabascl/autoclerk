bb803957081039f06f85e8e333bbdd85ad6e8012
Tabascl <simon@wildesw.com>
2020-10-01 21:31:43 +0200

    Restore fetchers

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -2,5 +2,4 @@ from fetchers.alternate import AlternateFetcher
 from fetchers.caseking import CasekingFetcher
 from fetchers.notebooksbilliger import NotebooksBilligerFetcher
 
-# FETCHER_LIST = [AlternateFetcher, CasekingFetcher, NotebooksBilligerFetcher]
-FETCHER_LIST = [NotebooksBilligerFetcher]
+FETCHER_LIST = [AlternateFetcher, CasekingFetcher, NotebooksBilligerFetcher]
